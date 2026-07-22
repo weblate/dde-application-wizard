b9ca5f1be3091264d0a2f6050f988754816896dc
BoneNI <bounkirdni@gmail.com>
2026-07-22 11:10:26 +0200

    Translated using Weblate (Lao)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: deepin/Application Wizard (dde-application-wizard)
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-application-wizard/lo/

--- translations/dde-application-wizard_lo.ts
+++ translations/dde-application-wizard_lo.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../dbus/launcher1compat.cpp" line="41"/>
         <source>%1 removed successfully</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 ລຶບອອກສຳເລັດແລ້ວ</translation>
     </message>
     <message>
         <location filename="../dbus/launcher1compat.cpp" line="43"/>
         <source>Failed to remove the app</source>
-        <translation type="unfinished"></translation>
+        <translation>ບໍ່ສາມາດລຶບແອັບໄດ້</translation>
     </message>
 </context>
 </TS>
